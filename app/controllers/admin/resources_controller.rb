b1b59873cf3808ee29c510bdd1f69ff22df70ad7
jfquarite <javier@assetec.org>
2011-11-04 14:03:55 -0500

    Update app/controllers/admin/resources_controller.rb

--- app/controllers/admin/resources_controller.rb
+++ app/controllers/admin/resources_controller.rb
@@ -23,7 +23,7 @@ class Admin::ResourcesController < Admin::BaseController
     respond_to do |format|
       format.html do
         set_default_action
-        add_resource_action("Trash", {:action => "destroy"}, {:confirm => "¿Esta seguro de #{Typus::I18n.t("Trash")}?", :method => 'delete'})
+        add_resource_action("Trash", {:action => "destroy"}, {:confirm => "#{Typus::I18n.t("Trash")}?", :method => 'delete'})
         generate_html
       end
 
